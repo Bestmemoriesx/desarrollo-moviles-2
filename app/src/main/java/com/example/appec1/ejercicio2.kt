720470c23e3a17abf54b87a46c947135c5a4f4db
Bestmemoriesx <147124390+Bestmemoriesx@users.noreply.github.com>
2024-06-11 02:49:56 -0500

    final ejercicio 2

--- app/src/main/java/com/example/appec1/ejercicio2.kt
+++ app/src/main/java/com/example/appec1/ejercicio2.kt
@@ -39,9 +39,70 @@ fun ejercicio2(){
             mutableStateOf("")
         }
 
+        var resultado by rememberSaveable {
+            mutableStateOf("")
+        }
+
         Column {
             Text(text = "CALCULAR NOTAS", textAlign = TextAlign.Center,
                 modifier= Modifier.fillMaxWidth()
             )
 
-}}}
+            OutlinedTextField(value = nota1, onValueChange = {nota1=it},
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "ingrese nota1") },
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+            )
+            OutlinedTextField(value = nota2, onValueChange = {nota2=it},
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "ingrese nota2") },
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+            )
+            OutlinedTextField(value = nota3, onValueChange = {nota3=it},
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "ingrese nota3") },
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+            )
+            OutlinedTextField(value = nota4, onValueChange = {nota4=it},
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "ingrese nota4") },
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+            )
+
+
+            Button(onClick = { resultado=calcularnota(nota1.toInt(),nota2.toInt(),nota3.toInt(),nota4.toInt())
+
+            }, modifier = Modifier.fillMaxWidth())
+            {
+                Text(text = "calcular notas")
+            }
+
+            Text(text = resultado)
+        }
+
+    }
+
+}
+
+fun calcularnota(nota1: Int,nota2: Int,nota3: Int,nota4: Int): String {
+    val notas = listOf(nota1.toDouble(), nota2.toDouble(), nota3.toDouble(), nota4.toDouble())
+    val notasOrden = notas.sorted()
+    val notasSinBaja = notasOrden.drop(1)
+    val promedio = notasSinBaja[0] * 0.2 + notasSinBaja[1] * 0.3 + notasSinBaja[2] * 0.5
+    return "el promedio del alumno es $promedio"
+
+
+
+}

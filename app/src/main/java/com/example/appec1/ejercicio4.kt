eb2d9e1820b7311b2892640a9513f87833370a36
Bestmemoriesx <147124390+Bestmemoriesx@users.noreply.github.com>
2024-06-11 03:40:16 -0500

    inicio ejercicio 4

---
+++ app/src/main/java/com/example/appec1/ejercicio4.kt
@@ -0,0 +1,19 @@
+package com.example.appec1
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ejercicio4(){
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(top = 30.dp)){
+
+
+    }
+    }
